edfc5987423fc293f730bd9afd458833c40985e5
masterfiras101 <masterfiras101@gmail.com>
2024-10-20 03:15:28 +0300

    update login component-checkbox

--- src/components/Checkbox.vue
+++ src/components/Checkbox.vue
@@ -4,31 +4,46 @@ import { computed } from 'vue';
 const emit = defineEmits(['update:checked']);
 
 const props = defineProps({
+    name: {
+    type: String,
+    required: true,
+  },
     checked: {
-        type: [Array, Boolean],
+        type: Boolean,
         required: true,
     },
     value: {
-        default: null,
-    },
+    type: String,
+    default: null,
+  },
 });
 
-// const proxyChecked = computed({
-//     get() {
-//         return props.checked;
-//     },
+const proxyChecked = computed({
+    get() {
+        return props.checked;
+    },
 
-//     set(val) {
-//         emit('update:checked', val);
-//     },
-// });
+    set(val) {
+        emit('update:checked', val);
+    },
+});
 </script>
 
 <template>
-    <input
+
+<div class="inline-flex items-center">
+  <label class="flex items-center cursor-pointer relative">
+    <input  class="peer h-5 w-5 cursor-pointer transition-all appearance-none rounded shadow hover:shadow-md border border-slate-300 checked:bg-#f472b6 checked:border-pink-600" 
         type="checkbox"
-        :value="value"
+        :value="props.value"
+        :name="props.name"
         v-model="proxyChecked"
-        class="rounded border-gray-300 text-pink-600 shadow-sm focus:ring-pink-500"
-    />
+    id="check8" />
+    <span class="absolute bg-[#f472b6] text-white opacity-0 peer-checked:opacity-100 top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
+    <svg xmlns="http://www.w3.org/2000/svg" class="h-3.5 w-3.5" viewBox="0 0 20 20" fill="currentColor" stroke="currentColor" stroke-width="1">
+      <path  fill-rule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clip-rule="evenodd"></path>
+    </svg>
+    </span>
+  </label>
+</div>
 </template>
--- src/views/pages/Login.vue
+++ src/views/pages/Login.vue
@@ -4,6 +4,8 @@ import InputLabel from '@/components/InputLabel.vue';
 import TextInput from '@/components/TextInput.vue';
 import ThemeToggle from '@/components/ThemeToggle.vue';
 
+import { ref } from 'vue';
+const remember = ref(true);
 </script>
 
 <template>
@@ -25,7 +27,10 @@ import ThemeToggle from '@/components/ThemeToggle.vue';
                                 </div> -->
                                 <!-- <span class="text-base font-bold text-blue-600 dark:text-white">OssolTrust أصول ترست</span> -->
                                 <div class="flex lg:flex-1 z-999 ml-7 items-center justify-center">
-                                    <p class=" text-gray-900 dark:font-bold font-semibold font-tharwat text-2xl  dark:text-white">أناقة فرحتي</p>
+                                    <!-- <p class=" text-gray-900 dark:font-bold font-semibold font-tharwat text-2xl  dark:text-white">أناقة فرحتي</p> -->
+                                 <h3 class="text-2xl font-semibold text-center text-gray-700 dark:text-white">قم بتسجيل الدخول</h3>
+
+                                    
                                 </div>
                             </router-link>
                             <div  class="hidden lg:hidden  justfy-start  lg:justify-end mt-1">
@@ -67,111 +72,119 @@ import ThemeToggle from '@/components/ThemeToggle.vue';
                 </div>
             </nav>
      </header>
-    <div class="m-auto xl:container px-12 sm:px-0 mx-auto bg-white/80 dark:bg-slate-900 text-black/50  dark:text-white/50">
-    <div class="mx-auto h-full sm:w-max ">
-    <div class="  py-12">
-      <div class="space-y-4">
-        <router-link to="">
-          <!-- <img src="images/tailus.svg" class="w-40 dark:hidden" alt="tailus logo" />
-          <img src="images/logo.svg" class="w-40 hidden dark:block" alt="tailus logo" /> -->
-        </router-link>
-      </div>
-    <div class="mt-12 rounded-2xl   bg-gray-50  dark:bg-slate-900 -mx-6 sm:-mx-10 p-8 sm:p-10">
-      <h3 class="text-2xl font-semibold text-center text-gray-700 dark:text-white">قم بتسجيل الدخول</h3>
-      
-                <form  class="mt-10  space-y-8 dark:text-white ">
-                <div class="mt-4">
-                    <InputLabel class="" for="password" value="اسم المستخدم" />
-                    <div class="flex justify-start items-center">
+     <div class="lg:flex lg:justify-center">
 
-                        <TextInput
-                        id="password"
-                        type="password"
-                        class="mt-1 block w-full text-black text-center"
-                        required
-                        placeholder="ادخل اسم المستخدم هنا"
-                        autocomplete="false"
-                        />
-                         <span class="absolute">
-                            <svg xmlns="http://www.w3.org/2000/svg" class="w-6 h-6 mx-3 text-gray-300 dark:text-gray-500"
-                                fill="#14B16F" viewBox="0 0 24 24" stroke="#14B16F" stroke-width="2">
-                                <path stroke-linecap="round" stroke-linejoin="round"
-                                d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z" />
-                            </svg>
-                            </span>
-                    </div>
-                        <!-- v-model="form.password" -->
-                    <!-- <InputError class="mt-2" :message="form.errors.password" /> -->
-                </div>
-                <div class="mt-4">
-                    <InputLabel class="" for="password" value="كلمة المرور" />
-                    <div class="flex justify-start items-center">
+         <div class="m-auto xl:container px-12 sm:px-0 mx-auto bg-white/80 dark:bg-slate-900 text-black/50  dark:text-white/50">
+         <div class="mx-auto h-full sm:w-max ">
+         <div class="  py-12">
+           <div class="space-y-4">
+             <router-link to="">
+               <!-- <img src="images/tailus.svg" class="w-40 dark:hidden" alt="tailus logo" />
+               <img src="images/logo.svg" class="w-40 hidden dark:block" alt="tailus logo" /> -->
+             </router-link>
+           </div>
+         <div class="mt-12 rounded-2xl   bg-gray-50  dark:bg-slate-900 -mx-6 sm:-mx-10 p-8 sm:p-10">
+           <!-- <h3 class="text-2xl font-semibold text-center text-gray-700 dark:text-white">قم بتسجيل الدخول</h3> -->
+           
+                     <form  class="mt-10  space-y-8 dark:text-white ">
+                       
+                     <div class="mt-4">
+                         <InputLabel class="" for="username" value="اسم المستخدم" />
+                         <div class="flex justify-start items-center relative">
+     
+                             <TextInput
+                             id="username"
+                             type="text"
+                             class="mt-1 block text-black text-center"
+                             required
+                             placeholder="ادخل اسم المستخدم هنا"
+                             autocomplete="false"
+                             />
+                              <span class="absolute -right-1">
+                                 <svg xmlns="http://www.w3.org/2000/svg" class="w-6 h-6 mx-3 text-gray-300 dark:text-gray-500"
+                                     fill="#f472b6" viewBox="0 0 24 24" stroke="##f472b6" stroke-width="2">
+                                     <path stroke-linecap="round" stroke-linejoin="round"
+                                     d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z" />
+                                 </svg>
+                                 </span>
+                         </div>
+                             <!-- v-model="form.password" -->
+                         <!-- <InputError class="mt-2" :message="form.errors.password" /> -->
+                     </div>
+                     <div class="mt-4">
+                         <InputLabel class="" for="password" value="كلمة المرور" />
+                         <div class="flex justify-start items-center relative">
+     
+                             <TextInput
+                             id="password"
+                             type="password"
+                             class="mt-1 block w-full text-black text-center"
+                             required
+                             placeholder="ادخل كلمة المرور هنا"
+                             autocomplete="false"
+                             />
+                             <span class="absolute -right-1">
+                             
+                                 <svg class="w-6 h-6 mx-3 text-gray-300 dark:text-gray-500" width="800px" height="800px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                 <path
+                                         d="M2 16C2 13.1716 2 11.7574 2.87868 10.8787C3.75736 10 5.17157 10 8 10H16C18.8284 10 20.2426 10 21.1213 10.8787C22 11.7574 22 13.1716 22 16C22 18.8284 22 20.2426 21.1213 21.1213C20.2426 22 18.8284 22 16 22H8C5.17157 22 3.75736 22 2.87868 21.1213C2 20.2426 2 18.8284 2 16Z"
+                                         fill="#f472b6" />
+                                 <path
+                                         d="M12 18C13.1046 18 14 17.1046 14 16C14 14.8954 13.1046 14 12 14C10.8954 14 10 14.8954 10 16C10 17.1046 10.8954 18 12 18Z"
+                                         fill="#FFFFFF" />
+                                 <path
+                                         d="M6.75 8C6.75 5.10051 9.10051 2.75 12 2.75C14.8995 2.75 17.25 5.10051 17.25 8V10.0036C17.8174 10.0089 18.3135 10.022 18.75 10.0546V8C18.75 4.27208 15.7279 1.25 12 1.25C8.27208 1.25 5.25 4.27208 5.25 8V10.0546C5.68651 10.022 6.18264 10.0089 6.75 10.0036V8Z"
+                                         fill="#f472b6" />
+                                 </svg>
+                             </span>
+                         </div>
+                             <!-- v-model="form.password" -->
+                         <!-- <InputError class="mt-2" :message="form.errors.password" /> -->
+                     </div>
+                 <div class="flex justify-between">
+     
+                     <router-link 
+                     :to="{name:'index_page'}"  class="-mr-3 w-max p-3 flex items-start">
+                     <span class="text-sm tracking-wide text-pink-600 dark:text-white"> نسيت كلمة المرور ؟ </span>
+                 </router-link>
+                 <label class="flex items-center">
+                     <span class="ms-2 mr-2 text-sm text-gray-600 text-black/50  dark:text-white"
+                         >تذكرني</span
+                     >
+                 <Checkbox name="remember" v-model:checked="remember" />
+     
+                 </label>
+                 <!-- <Checkbox name="remember" v-model:checked="form.remember" /> -->
+             <!-- <div class="w-full relative before:absolute before:bottom-0 before:h-0.5 before:left-0 before:origin-right focus-within:before:origin-left before:right-0 before:scale-x-0 before:m-auto before:bg-pink-400 dark:before:bg-pink-800 focus-within:before:!scale-x-100 focus-within:invalid:before:bg-red-400 before:transition before:duration-300">
+               <input id="" type="Your password" placeholder="Your answer" class="w-full bg-transparent pb-3  border-b border-gray-300 dark:placeholder-gray-300 dark:border-gray-600 outline-none  invalid:border-red-400 transition">
+             </div> -->
+             </div>
+     
+             <div class="block items-center justify-center">
+             <button
+                 class="w-full rounded-md bg-pink-500  border border-pink-400 dark:bg-slate-900  h-11 flex items-center justify-center px-6 py-3 transition hover:bg-pink-600 focus:bg-pink-600 active:bg-pink-800"
+                 >
+                 <span class="text-base font-semibold text-white">دخول</span>
+             </button>
+             <!-- :class="{ 'opacity-25': form.processing }" -->
+             <!-- :disabled="form.processing" -->
+             <router-link :to="{name:'index_page'}" type="reset" class=" border mt-1 p-3 rounded-md flex justify-center text-center">
+                 <span class="text-sm text-center   items-center   tracking-wide text-pink-600 dark:text-white">إنشاء حساب</span>
+             </router-link>
+         </div>
+     </form>
+     </div>
+     
+     </div>
+     </div>
+             </div>
+             <div class="overflow-hidden absolute hidden lg:flex scale-x-[-1] lg:w-3/12 left-[10%]">
+                    <img src="@/assets/imgs/png/ic_bride_girl.png" alt="masterfiras101 logo" height="" width="">
+            </div>
+     </div>
+</div>
+</div>
 
-                        <TextInput
-                        id="password"
-                        type="password"
-                        class="mt-1 block w-full text-black text-center"
-                        required
-                        placeholder="ادخل كلمة المرور هنا"
-                        autocomplete="false"
-                        />
-                        <span class="absolute">
-                        
-                            <svg class="w-6 h-6 mx-3 text-gray-300 dark:text-gray-500" width="800px" height="800px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                            <path
-                                    d="M2 16C2 13.1716 2 11.7574 2.87868 10.8787C3.75736 10 5.17157 10 8 10H16C18.8284 10 20.2426 10 21.1213 10.8787C22 11.7574 22 13.1716 22 16C22 18.8284 22 20.2426 21.1213 21.1213C20.2426 22 18.8284 22 16 22H8C5.17157 22 3.75736 22 2.87868 21.1213C2 20.2426 2 18.8284 2 16Z"
-                                    fill="#14B16F" />
-                            <path
-                                    d="M12 18C13.1046 18 14 17.1046 14 16C14 14.8954 13.1046 14 12 14C10.8954 14 10 14.8954 10 16C10 17.1046 10.8954 18 12 18Z"
-                                    fill="#FFFFFF" />
-                            <path
-                                    d="M6.75 8C6.75 5.10051 9.10051 2.75 12 2.75C14.8995 2.75 17.25 5.10051 17.25 8V10.0036C17.8174 10.0089 18.3135 10.022 18.75 10.0546V8C18.75 4.27208 15.7279 1.25 12 1.25C8.27208 1.25 5.25 4.27208 5.25 8V10.0546C5.68651 10.022 6.18264 10.0089 6.75 10.0036V8Z"
-                                    fill="#14B16F" />
-                            </svg>
-                        </span>
-                    </div>
-                        <!-- v-model="form.password" -->
-                    <!-- <InputError class="mt-2" :message="form.errors.password" /> -->
-                </div>
-            <div class="flex justify-between">
-
-                <router-link 
-                :to="{name:'index_page'}"  class="-mr-3 w-max p-3 flex items-start">
-                <span class="text-sm tracking-wide text-pink-600 dark:text-white"> نسيت كلمة المرور ؟ </span>
-            </router-link>
-            <label class="flex items-center">
-                <span class="ms-2 mr-2 text-sm text-gray-600 text-black/50  dark:text-white"
-                    >تذكرني</span
-                >
-            <Checkbox name="remember"  />
-
-            </label>
-            <!-- <Checkbox name="remember" v-model:checked="form.remember" /> -->
-        <!-- <div class="w-full relative before:absolute before:bottom-0 before:h-0.5 before:left-0 before:origin-right focus-within:before:origin-left before:right-0 before:scale-x-0 before:m-auto before:bg-pink-400 dark:before:bg-pink-800 focus-within:before:!scale-x-100 focus-within:invalid:before:bg-red-400 before:transition before:duration-300">
-          <input id="" type="Your password" placeholder="Your answer" class="w-full bg-transparent pb-3  border-b border-gray-300 dark:placeholder-gray-300 dark:border-gray-600 outline-none  invalid:border-red-400 transition">
-        </div> -->
-        </div>
-
-        <div class="block items-center justify-center">
-        <button
-            class="w-full rounded-md bg-pink-500  border border-pink-400 dark:bg-slate-900  h-11 flex items-center justify-center px-6 py-3 transition hover:bg-pink-600 focus:bg-pink-600 active:bg-pink-800"
-            >
-            <span class="text-base font-semibold text-whit">دخول</span>
-        </button>
-        <!-- :class="{ 'opacity-25': form.processing }" -->
-        <!-- :disabled="form.processing" -->
-        <router-link :to="{name:'index_page'}" type="reset" class=" border mt-1 p-3 rounded-md flex justify-center text-center">
-            <span class="text-sm text-center   items-center   tracking-wide text-pink-600 dark:text-white">إنشاء حساب</span>
-        </router-link>
-          </div>
-                    </form>
-    </div>
-
-    </div>
-    </div>
-    </div>
-    </div>
-    </div>
 
 
 </template>
